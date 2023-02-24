87977bb2f0b51d8245c5849ac10d32d304d6ae85
Nicolás Muros <nicomuros@gmail.com>
2023-02-24 20:42:58 -0300

    Icono agregado y arreglo nombre

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -4,18 +4,19 @@ import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import CartWidget from "../CartWidget/CartWidget";
+import {GiHamburger} from 'react-icons/gi';
 import "./CustomNavbar.css"
 
   const CustomNavbar = () => {
     return (
-      <Navbar bg="dark" variant="dark" expand="md">
+      <Navbar bg="dark" variant="dark" expand="md" class="align-items-center">
         <Container>
-          <Navbar.Brand href="#home">Rock n' Burger</Navbar.Brand>
+          <Navbar.Brand href="#home" className="d-flex align-items-center"><GiHamburger /> <span className="ms-2">Rock'n Burger</span></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <NavbarCollapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <NavDropdown title="Categorías" id="collasible-nav-dropdown">
+              <NavDropdown title="Categorías" id="collasible-nav-dropdown"  className="d-flex align-items-center">
                 <NavDropdown.Item href="#action/3.1">Comida</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Bebidas</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Postres</NavDropdown.Item>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,14 +3,10 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const ItemListContainer = (props) => {
-  return <ItemListComponent greeting={props.greeting} />;
-}
-
-const ItemListComponent = (props) => {
     return <Container>
-        <Row>
-            <Col><h1 class="display-1">{props.greeting}</h1></Col>
-        </Row>
-    </Container>
+    <Row>
+        <Col><h1 class="display-1">{props.greeting}</h1></Col>
+    </Row>
+</Container>
 }
 export default ItemListContainer
