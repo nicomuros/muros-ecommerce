f525817441024af3465e557bed789a68d0f186c9
Nicolás Muros <nicomuros@gmail.com>
2023-02-24 17:19:56 -0300

    PreEntrega1+Muros

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import CustomNavbar from "./components/CustomNavbar/CustomNavbar"
-
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 
 function App() {
   return (
     <>
     <CustomNavbar />
+    <ItemListContainer greeting="Bienvenidos!" />
     </>
   )
 }
