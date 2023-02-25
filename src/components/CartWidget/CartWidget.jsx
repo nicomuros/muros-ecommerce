a832f18f981c4519d3b710288fe67bc0e988bc35
Nicolás Muros <nicomuros@gmail.com>
2023-02-25 19:09:27 -0300

    Botones Navbar terminados

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import { BsCart4 } from 'react-icons/bs';
 
 const CartWidget = () => {
   return (
-    <i style={{fontSize: "30px"}}>
+    <i style={{fontSize: "30px", display: "flex", alignItems: "center"}}>
       <BsCart4 />
     </i>
   );
--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -7,31 +7,59 @@ import CartWidget from "../CartWidget/CartWidget";
 import { GiHamburger } from "react-icons/gi";
 import "./CustomNavbar.css";
 //TODO
-    /*
+/*
     Customizar navbar en casa breakpoint, para eso estaba pensando en buscar que clases representan los breakpoints
     y ver si al customizarlas desde css unicamente se afecta ese breakpoint y no toda la barra (xl, md, xs)
     */
 const CustomNavbar = () => {
   return (
-      <Navbar bg="dark" variant="dark" expand="md" className="fixed-top">
-      <Container>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      className="fixed-top res-xl res-lg res-md res-sm mynav"
+    >
+      <Container className="d-flex align-items-center">
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand className="d-md-none">
+          <Button variant="dark" href="#action/3.5">
+            <GiHamburger style={{ fontSize: "2rem" }} className="text-center" />
+          </Button>
+        </Navbar.Brand>
+        <Nav className="d-md-none">
+          <Button variant="dark" href="#cart">
+            <CartWidget />
+          </Button>
+        </Nav>
+
         <NavbarCollapse id="responsive-navbar-nav" className="navcol">
-          <Nav fill className="navigator" >
-            <Button variant="dark" href="#home">Home</Button>
-            <Button variant="dark" href="#action/3.1">Menu</Button>
-            <Button variant="dark" href="#action/3.5">
-              <Navbar.Brand><GiHamburger style={{fontSize: "2rem"}} className="text-center"/></Navbar.Brand>
+          <Nav fill className="navigator">
+            <Button variant="dark" href="#home">
+              Home
             </Button>
-            <Button variant="dark" href="#action/3.3">Contactanos</Button>
-            <Button variant="dark" href="#asd">
+            <Button variant="dark" href="#action/3.1">
+              Menu
+            </Button>
+            <Button
+              variant="dark"
+              href="#action/3.5"
+              className="d-none d-md-block"
+            >
+              <GiHamburger
+                style={{ fontSize: "2rem" }}
+                className="text-center"
+              />
+            </Button>
+            <Button variant="dark" href="#action/3.3">
+              Contactanos
+            </Button>
+            <Button variant="dark" href="#cart" className="d-none d-md-block">
               <CartWidget />
             </Button>
           </Nav>
         </NavbarCollapse>
       </Container>
     </Navbar>
-    
   );
 };
 
