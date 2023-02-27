231b6ca5f544b932eda44f1ddba0ec6888ad69fc
Nicolás Muros <nicomuros@gmail.com>
2023-02-27 01:50:09 -0300

    Terminado

--- src/components/TextImg/TextImg.jsx
+++ src/components/TextImg/TextImg.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 
-const TextImg = (props) => {
+const TextImg = () => {
   return (
     <Container fluid>
         <Row m={0}>
