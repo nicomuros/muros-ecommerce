d00496dcfaecd4caf0144a6dc55a5455aa24f0f2
Nicolás Muros <nicomuros@gmail.com>
2023-02-27 02:15:35 -0300

    Separador terminado

--- src/components/Separador/Separador.jsx
+++ src/components/Separador/Separador.jsx
@@ -5,8 +5,8 @@ import styles from './Separador.module.css'
 
 const Separador = () => {
   return (
-    <Container fluid className={`${styles.container} d-flex align-items-center justify-content-center`}>
-        <h2 className={`${styles.text} py-5 display-1 fw-bolder`}>ROCK'N BURGER</h2>
+    <Container fluid className={`${styles.container}  d-flex align-items-center justify-content-center`}>
+        <h2 className={`${styles.text} py-5 my-5 display-1 fw-bolder`}>ROCK'N BURGER</h2>
     </Container>
   )
 }
