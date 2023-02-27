4899d5ea5d68266eca366f847f6fa1ee4a8fc30e
Nicolás Muros <nicomuros@gmail.com>
2023-02-27 02:09:47 -0300

    añadido separador

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import CustomNavbar from "./components/CustomNavbar/CustomNavbar"
 import Welcome from "./components/Welcome/Welcome";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+//import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import TextImg from "./components/TextImg/TextImg";
+import Separador from "./components/Separador/Separador";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <CustomNavbar />
     <Welcome />
     <TextImg />
+    <Separador />
     </>
   )
 }
---
+++ src/components/Separador/Separador.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import styles from './Separador.module.css'
+
+
+const Separador = () => {
+  return (
+    <Container fluid className={`${styles.container} d-flex align-items-center justify-content-center`}>
+        <h2 className={`${styles.text} py-5 display-1 fw-bolder`}>ROCK'N BURGER</h2>
+    </Container>
+  )
+}
+
+export default Separador
