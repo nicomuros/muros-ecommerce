1a15966fabdc4c1348caf756c8bf9d12f0a46f8d
Nicolás Muros <nicomuros@gmail.com>
2023-02-27 23:16:33 -0300

    Estilizados los separadores

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import {v4 as uuidv4} from 'uuid';
 import Item from "../Item/Item";
+import ItemCount from "../ItemCount/ItemCount";
+import styles from "./ItemList.module.css"
 
 const ItemList = ( {items=[] } ) => {
   const carneProducts = items.filter((curr) => curr.categoria === "carne");
@@ -9,24 +11,25 @@ const ItemList = ( {items=[] } ) => {
   const fritasProducts = items.filter((curr) => curr.categoria === "fritas");
   return (
     <Container><Row>
-      <h1>Carne</h1>
+      <h1 className={`${styles.titulo} text-center`}>CARNE</h1>
       {
          carneProducts.map((item) => {
           return <Item key={item.id} item={item} />
          })
       }
-      <h1>Fritas</h1>
+      <h1 className={`${styles.titulo} text-center`}>PAPAS FRITAS</h1>
       {
          fritasProducts.map((item) => {
           return <Item key={item.id} item={item} />
          })
       }
-      <h1>Veggies</h1>
+      <h1 className={`${styles.titulo} text-center`}>VEGGIES</h1>
       {
          veggieProducts.map((item) => {
           return <Item key={item.id} item={item} />
          })
       }
+      <ItemCount stock={5} initial={1}/>
     </Row></Container>
   );
 };
