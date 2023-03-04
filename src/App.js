e4d479cc4188ff7a508c5e7bdbb08a5c4e1300ef
Nicolás Muros <nicomuros@gmail.com>
2023-03-04 13:29:17 -0300

    Consumiendo API via GET y POST

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Welcome from "./components/Welcome/Welcome";
 //import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import TextImg from "./components/TextImg/TextImg";
 import Separador from "./components/Separador/Separador";
-import ItemCount from "./components/ItemCount/ItemCount";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ConsumiendoApi from "./components/ConsumiendoApi/ConsumiendoApi";
 
--- src/components/ConsumiendoApi/ConsumiendoApi.jsx
+++ src/components/ConsumiendoApi/ConsumiendoApi.jsx
@@ -1,13 +1,46 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const ConsumiendoApi = () => {
+
+    /*
+    //PETICIÓN TIPO GET
+
+    const [ post, setPost ] = useState([]);
+
+    //Tengo que manejar la promesa del fetch y despues la promesa del json
     useEffect(( ) => {
         const getData = fetch("https://jsonplaceholder.typicode.com/posts");
         getData
             .then( response => response.json())
-            .then( res => console.log(res))
+            .then( res => setPost(res))
             .catch( error => console.log("error: " + error))
-    },[])
+            .finally(console.log("No importa como termine, yo me ejecuto"))
+    },[]); 
+
+   console.log(post)
+    */
+    //PETICIÓN TIPO POST
+    const [isCreated, setIsCreated ] = useState(false);
+    //Tengo que manejar la promesa del fetch y despues la promesa del json
+    useEffect(( ) => {
+        const createPost = fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            //envio cadena de texto, el backend la recibe, la convierte a objeto y la manipula
+            body: JSON.stringify({
+                title: "esta es una prueba de front",
+                userId: 2,
+                body: "prueba del verbo post"
+            })
+        });
+        createPost
+            .then( response => setIsCreated(true) )
+            
+    },[]); 
+
+    console.log(isCreated)
   return (
     <div>
         API CONNECTION
