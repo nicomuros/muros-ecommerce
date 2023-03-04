d8c56e9608a016ae4c2bd734d193c464284266dc
Nicolás Muros <nicomuros@gmail.com>
2023-03-04 16:49:14 -0300

    Componetización "Home"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import CustomNavbar from "./components/CustomNavbar/CustomNavbar"
 import Welcome from "./components/Welcome/Welcome";
-//import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import TextImg from "./components/TextImg/TextImg";
-import Separador from "./components/Separador/Separador";
-import ItemCount from "./components/ItemCount/ItemCount";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
+
 
 function App() {
   return (
     <>
     <CustomNavbar />
     <Welcome />
-    <TextImg />
-    <Separador text="ROCK'N BURGER"/>
-    <ItemListContainer />
+
+    
+    
     </>
   )
 }
