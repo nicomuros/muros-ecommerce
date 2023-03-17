03289962dc36b61ec52704fef4a4e24cef3377aa
Nicolás Muros <nicomuros@gmail.com>
2023-03-17 14:13:30 -0300

    empezando

---
+++ src/components/utils/Categorizador.js
@@ -0,0 +1,37 @@
+import Separador from "../Separador/Separador";
+import { v4 as uuidv4 } from "uuid";
+export function Categorizador(items) {
+  const categorizados = items
+    ? items
+
+        .sort((a, b) => {
+          if (a.categoria === b.categoria) {
+            return b.id - a.id; // Ordenar por id dentro de la misma categoría
+          }
+          return a.categoria > b.categoria ? 1 : -1; // Ordenar por categoría
+        })
+        .reduce(
+          (acc, curr) => {
+            if (curr.categoria !== acc[1]) {
+              acc = [
+                [
+                  ...acc[0],
+                  {type: "titulo",
+                  titulo: curr.categoria},
+                  curr,
+                ], curr.categoria,
+              ];
+            } else {
+              acc = [[...acc[0], curr], curr.categoria];
+            }
+            return acc;
+          },
+          [[], null]
+        )
+    : [];
+  console.log("es aca -> " + categorizados)
+  return categorizados
+  
+  
+  ;
+}
