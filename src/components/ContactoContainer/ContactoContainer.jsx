5eae0166e053d8a570a5330ccbfb7c91581b2543
Nicolás Muros <nicomuros@gmail.com>
2023-03-17 23:58:31 -0300

    añadidos mensajes de error a los forms de contacto

--- src/components/ContactoContainer/ContactoContainer.jsx
+++ src/components/ContactoContainer/ContactoContainer.jsx
@@ -11,30 +11,55 @@ const ContactoContainer = () => {
     userMessage: "",
   });
 
+  //control de renderización del DOM
+  const [anyError, setAnyError] = useState({
+    userNameError: false,
+    userEmailError: false,
+    userPhoneError: false,
+    userMessageError: false,
+  });
+
+  //control de mensaje enviado
+  const [sended, setSended] = useState(false);
+
   const handleChange = (event) => {
     setUserData({ ...userData, [event.target.name]: event.target.value });
+    setAnyError({ ...anyError, [`${event.target.name}Error`]: false }); //limpio mensaje de error
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (userData.userName.length < 4 || userData.userName.length > 15)
-      console.log("Largo de nombre invalido");
-    else if (!userData.userEmail.includes("@gmail.com"))
-      console.log("correo inválido");
-    else if (userData.userPhone.lenght > 10)
-      console.log("largo del telefono invalido");
-    else if (userData.userMessage === "") console.log("no escribió el mensaje");
-    else console.log("mensaje enviado");
+    let error = false;
+    if (userData.userName.length < 4 || userData.userName.length > 15) {
+      setAnyError((prevState) => ({ ...prevState, userNameError: true }));
+      error = true;
+    }
+    if (!userData.userEmail.includes("@gmail.com")) {
+      setAnyError((prevState) => ({ ...prevState, userEmailError: true }));
+      error = true;
+    }
+    if (userData.userPhone.length !== 10) {
+      console.log("error")
+      setAnyError((prevState) => ({ ...prevState, userPhoneError: true }));
+      error = true;
+    }
+    if (!userData.userMessage || userData.userMessage.trim() === "") {
+      setAnyError((prevState) => ({ ...prevState, userMessageError: true }));
+      error = true;
+    }
+    if (!error) {
+      setSended(true);
+    }
   };
 
-
+  console.log(anyError);
   return (
     <Container fluid className={styles.container}>
       <Row className="d-flex justify-content-center">
         <Col lg={5} md={6}>
           <Separador categoryName="Contactanos" />
           <form onSubmit={handleSubmit}>
-          <div className={styles.inputContainer}>
+            <div className={styles.inputContainer}>
               <label htmlFor="userNameInput">Nombre:</label>
               <input
                 type="text"
@@ -45,6 +70,11 @@ const ContactoContainer = () => {
                 name="userName"
                 onChange={handleChange}
               />
+              {anyError.userNameError && (
+                <p className={styles.errorMessage}>
+                  Ingrese un nombre válido, debe contener entre 4 y 15 caracteres
+                </p>
+              )}
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="userEmailInput">Correo electrónico:</label>
@@ -57,12 +87,17 @@ const ContactoContainer = () => {
                 name="userEmail"
                 onChange={handleChange}
               />
+              {anyError.userEmailError && (
+                <p className={styles.errorMessage}>
+                  Ingrese un correo válido, debe contener @gmail.com
+                </p>
+              )}
             </div>
 
             <div className={styles.inputContainer}>
               <label htmlFor="userPhoneInput">Teléfono:</label>
               <input
-                type="number"
+                type="text"
                 className="form-control"
                 id="userPhoneInput"
                 aria-describedby="numberInput"
@@ -70,6 +105,11 @@ const ContactoContainer = () => {
                 name="userPhone"
                 onChange={handleChange}
               />
+              {anyError.userPhoneError && (
+                <p className={styles.errorMessage}>
+                  Ingrese un número válido, debe contener 10 caracteres
+                </p>
+              )}
             </div>
 
             <div className={styles.inputContainer}>
@@ -82,6 +122,11 @@ const ContactoContainer = () => {
                 name="userMessage"
                 onChange={handleChange}
               />
+              {anyError.userMessageError && (
+                <p className={styles.errorMessage}>
+                  Ingrese un mensaje
+                </p>
+              )}
             </div>
 
             <div className={styles.buttonContainer}>
@@ -90,6 +135,11 @@ const ContactoContainer = () => {
                 type="submit"
                 paddingReceived="0px 100px"
               />
+              {sended && (
+                <p className={styles.successMessage}>
+                  Mensaje enviado
+                </p>
+              )}
             </div>
           </form>
         </Col>
