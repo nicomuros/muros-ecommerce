833a1ec39d98ae87694887d17cca5096e7c2907e
Nicolás Muros <nicomuros@gmail.com>
2023-03-20 21:09:37 -0300

    limpieza categorizador

--- src/components/utils/Categorizador.js
+++ src/components/utils/Categorizador.js
@@ -1,4 +1,30 @@
-export function Categorizador(items) {
+export function Categorizador(items = []) {
+  const itemsOrdenados = sortItems(items)
+  const itemsTitulados = addTitles(itemsOrdenados)
+  console.log(itemsTitulados)
+  return itemsTitulados
+}
+  
+const sortItems = (items) => {
+  return items.sort((currProduct, nextProduct) => {
+    if (currProduct.categoria === nextProduct.categoria) {
+      return nextProduct.id - currProduct.id; // Ordenar por id dentro de la misma categoría
+    }
+    return currProduct.categoria > nextProduct.categoria ? 1 : -1; // Ordenar por categoría
+  })
+}
+  
+const addTitles = (itemsOrdenados) => {
+  return itemsOrdenados.reduce((acc, item, index) => {
+    const prevCategory = index > 0 && itemsOrdenados[index - 1].categoria
+    if (index === 0 || item.categoria !== prevCategory){
+      acc = [...acc, {type: "titulo", titulo: item.categoria}]
+    }
+    return [...acc, item]
+    
+  }, [])
+}
+  /*
   const categorizados = items
     ? items
 
@@ -28,8 +54,6 @@ export function Categorizador(items) {
           [[], null]
         )
     : [];
-    return categorizados[0]
-  
-  
-  ;
-}
+    return categorizados[0];
+    */
+
