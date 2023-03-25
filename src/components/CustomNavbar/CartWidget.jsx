ebee1c37b2772fd88277a161942afa76997454e8
Nicolás Muros <nicomuros@gmail.com>
2023-03-24 21:33:40 -0300

    icono cart con numeros y estilizado

--- src/components/CustomNavbar/CartWidget.jsx
+++ src/components/CustomNavbar/CartWidget.jsx
@@ -1,20 +1,25 @@
 import { BsCart4 } from 'react-icons/bs';
 import { useContext } from 'react'; 
 import { CartContext } from '../../context/CartContext';
+import styles from './CartWidget.module.css'; // Importar archivo CSS
 
 const CartWidget = () => {
-  const {getCartTotalQuantity} = useContext(CartContext)
-  const totalProducts = getCartTotalQuantity()
+  const {getCartTotalQuantity} = useContext(CartContext);
+  const totalProducts = getCartTotalQuantity();
 
   return (
-    <i style={{fontSize: "30px", display: "flex", alignItems: "center"}}>
-      {totalProducts}
-      <BsCart4 />
-    </i>
+    <div className={styles.cartWrapper}>
+      <div className={styles.cartIcon}>
+        <BsCart4 />
+      </div>
+      {totalProducts > 0 && ( // Mostrar el contador solo si hay productos en el carrito
+        <div className={styles.productCount}>
+          {totalProducts}
+        </div>
+      )}
+    </div>
   );
 };
 
 export default CartWidget;
-
-
 
