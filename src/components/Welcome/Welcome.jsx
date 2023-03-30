8eb8501d162f8b1750082aadc99506d85284100d
Nicolás Muros <nicomuros@gmail.com>
2023-03-29 22:53:11 -0300

    Creado separador y actualizado welcome

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,25 +1,27 @@
 import { Button, Carousel, Col, Container, Row } from "react-bootstrap";
+import ItemListContainer from "../ItemListContainer/ItemListContainer";
+import Separador from "../Separador/Separador";
 import "./Welcome.css";
 const Welcome = () => {
   return (
     <>
     <Carousel interval={2000} fade>
       <Carousel.Item>
-        <img className="d-block w-100 bg-img1" alt="First slide" />
+        <img className="d-block w-100 bg-img1" alt="" />
         <Carousel.Caption>
           <h3 className="display-1 text-sm text-md text-lg">ROCK</h3>
           <p className="h5 text-sm text-md text-lg">Y si no es rock, que no sea nada</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100 bg-img2" alt="First slide" />
+        <img className="d-block w-100 bg-img2" alt="" />
         <Carousel.Caption>
           <h3 className="display-1 text-sm text-md text-lg">&</h3>
           <p className="h5 text-sm text-md text-lg">Vení, disfrutá, reite y viví momentos únicos</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100 bg-img3" alt="First slide" />
+        <img className="d-block w-100 bg-img3" alt="" />
         <Carousel.Caption>
           <h3 className="display-1 text-sm text-md text-lg">BURGERS</h3>
           <p className="h5 text-sm text-md text-lg">Como ninguna que hayas probado</p>
@@ -41,6 +43,8 @@ const Welcome = () => {
             </Col>
         </Row>
     </Container>
+    <Separador title={"Nuestro menu"}/>
+    <ItemListContainer />
     </>
   );
 };
