16b470c5be6947d1c659be3d2d96985b6d506661
Nicolás Muros <nicomuros@gmail.com>
2023-04-03 23:59:40 -0300

    Correjido item

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -4,36 +4,28 @@ import { Link } from "react-router-dom";
 import styles from "./Item.module.css";
 
 const Item = ({ item }) => {
-
   return (
-    <Col lg={6} style={{
-    }}>
+    <Col lg={6}>
       <Link to={`/item/${item.hostId}/${item.name}`} className={styles.link}>
         <Card
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            backgroundColor: "transparent",
-            margin: "10px",
-            minWidth: "160px",
-            border: "0px"
-          }} className={styles.cardCustom}
+          className={`${styles.cardCustom} border-0`}
+          style={{ backgroundColor: "transparent", margin: "10px" }}
         >
-          <div className="aspect-ratio-square">
-            <Card.Img variant="left" className={styles.imagen} src={item.img} />
-          </div>
-          <div className="pl-3">
-            <Card.Body>
-              <Card.Text className={` ${styles.titulo}`}>{item.name}</Card.Text>
-              <Card.Text className={styles.texto}>
-                {item.ingredients.join(" - ")}
-              </Card.Text>
-              <Card.Text className={styles.precio}>
-                ${item.price}
-              </Card.Text>
-            </Card.Body>
+          <div className="d-flex justify-content-center justify-content-md-start align-items-center flex-column flex-md-row">
+            <div className="aspect-ratio-square">
+              <Card.Img variant="left" className={styles.imagen} src={item.img} />
+            </div>
+            <div className="pl-3">
+              <Card.Body>
+                <Card.Text className={styles.titulo}>{item.name}</Card.Text>
+                <Card.Text className={styles.texto}>
+                  {item.ingredients.join(" - ")}
+                </Card.Text>
+                <Card.Text className={styles.precio}>
+                  ${item.price}
+                </Card.Text>
+              </Card.Body>
+            </div>
           </div>
         </Card>
       </Link>
