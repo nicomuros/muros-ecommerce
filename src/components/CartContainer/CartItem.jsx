815e891864a85c26127d54fed13343e77bff1bf3
Nicolás Muros <nicomuros@gmail.com>
2023-04-04 00:06:02 -0300

    cartitem responsive

--- src/components/CartContainer/CartItem.jsx
+++ src/components/CartContainer/CartItem.jsx
@@ -1,56 +1,32 @@
 import React from "react";
-import styles from "./Cart.module.css"
+import styles from "./Cart.module.css";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import CustomButton from "../CustomButton/CustomButton";
 
-const CartItem = ({producto, handleMinus, handlePlus, handleDeleteProduct}) => {
+const CartItem = ({ producto, handleMinus, handlePlus, handleDeleteProduct }) => {
   return (
-    <div className={styles.productContainer}>
+    <div className={`d-flex flex-column flex-sm-row ${styles.productContainer}`}>
       <div className={styles.productInfoContainer}>
-
         <div className={styles.productImgContainer}>
-          <img
-            src={producto.img}
-            alt="product-img"
-            className={styles.productImg}
-          />
+          <img src={producto.img} alt="product-img" className={styles.productImg} />
         </div>
-
         <div className={styles.productText}>
           <h5>{producto.name}</h5>
           <h6>$ {producto.price}</h6>
         </div>
       </div>
-
-      <div className={styles.productButtons}>
-        <div className={styles.productButtons}>
-          <div className={styles.productCounter}>
-            <span
-              className={styles.minus}
-              onClick={() => {
-                handleMinus(producto.id);
-              }}>
-              <AiOutlineMinus />
-            </span>
-
-            <span className={styles.num}>{producto.quantity}</span>
-
-            <span
-              className={styles.plus}
-              onClick={() => {
-                handlePlus(producto.id);
-              }}>
-              
-              <AiOutlinePlus />
-            </span>
-          </div>
-          <div
-            onClick={() => {
-              handleDeleteProduct(producto.id);
-            }}
-          >
-            <CustomButton text="Eliminar" paddingReceived="0px 20px" />
-          </div>
+      <div className={`mt-3 mt-md-0 ${styles.productButtons} order-md-2`}>
+        <div className={styles.productCounter}>
+          <span className={styles.minus} onClick={() => { handleMinus(producto.id); }}>
+            <AiOutlineMinus />
+          </span>
+          <span className={styles.num}>{producto.quantity}</span>
+          <span className={styles.plus} onClick={() => { handlePlus(producto.id); }}>
+            <AiOutlinePlus />
+          </span>
+        </div>
+        <div onClick={() => { handleDeleteProduct(producto.id); }}>
+          <CustomButton text="Eliminar" paddingReceived="0px 20px" />
         </div>
       </div>
     </div>
